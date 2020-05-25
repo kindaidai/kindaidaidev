f9a306a72c798e4d1019f255cc459baca9fef864
kindaidai <kindai.0911@gmail.com>
2020-05-25 22:28:43 +0900

    yarn add --dev prettier eslint-config-prettier eslint-plugin-prettier pretty-quick and add .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,27 @@
 "use strict";
 
 module.exports = {
-    "env": {
-      "browser": true
+  env: {
+    browser: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-      "airbnb",
-      "airbnb/hooks",
-      "plugin:@typescript-eslint/eslint-recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
-    },
-    "plugins": [
-      "@typescript-eslint"
-    ],
-    "rules": {
-      "no-undef": "error"
-    }
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "no-undef": "error",
+    "prettier/prettier": "error",
+  },
 };

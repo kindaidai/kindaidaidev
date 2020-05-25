6f5193526b82f1c1fef9fd2a1dd1b91c2ca4342b
kindaidai <kindai.0911@gmail.com>
2020-05-25 21:59:48 +0900

    modify .eslintrc.js for airbnb and typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,29 +2,25 @@
 
 module.exports = {
     "env": {
-        "browser": true,
-        "es6": true
+      "browser": true
     },
     "extends": [
-        "airbnb",
-        "airbnb/hooks",
-        "plugin:@typescript-eslint/eslint-recommended"
+      "airbnb",
+      "airbnb/hooks",
+      "plugin:@typescript-eslint/eslint-recommended"
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
     },
     "plugins": [
-        "@typescript-eslint"
+      "@typescript-eslint"
     ],
     "rules": {
+      "no-undef": "error"
     }
 };

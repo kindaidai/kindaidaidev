a20860d9cdab448f9b4d5f7c181dfd773d1030cf
kindaidai <kindai.0911@gmail.com>
2020-06-09 21:27:56 +0900

    remove @fullhuman/postcss-purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,3 @@
-// const purgecss = [
-//   '@fullhuman/postcss-purgecss',
-//   {
-//     content: ['./components/**/*.js', './pages/**/*.js'],
-//     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-//   },
-// ]
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    // ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
-  ],
+  plugins: ['tailwindcss'],
 }

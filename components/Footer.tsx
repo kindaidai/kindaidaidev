6c60febe22b363317a46e0d10e1f710a6f513330
kindaidai <kindai.0911@gmail.com>
2020-06-09 22:45:56 +0900

    add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer: React.FC = () => (
+  <footer>
+    <div className="bg-gray-200">
+      <div className="container mx-auto py-4 px-5">
+        <p className="text-gray-500 text-sm text-center sm:text-center">
+          © 2020 kindaidaidev
+        </p>
+      </div>
+    </div>
+  </footer>
+)
+
+export default Footer
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,54 +1,16 @@
 import { AppProps } from 'next/app'
-import Icon from '../components/header/Icon'
-import NavLink from '../components/header/NavLink'
 import '../styles.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <header className="text-gray-600 body-font border-b border-gray-1000">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Icon />
-          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <NavLink name="about" />
-            <NavLink name="blog" />
-          </nav>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            <a
-              className="ml-3 text-gray-500"
-              href="https://twitter.com/kindai_dai"
-            >
-              <svg
-                fill="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                className="w-5 h-5"
-                viewBox="0 0 22 22"
-              >
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-              </svg>
-            </a>
-            <a
-              className="ml-3 text-gray-500"
-              href="https://github.com/kindaidai"
-            >
-              <svg
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                className="w-5 h-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
-                />
-              </svg>
-            </a>
-          </span>
-        </div>
-      </header>
-      <Component {...pageProps} />
-    </>
+    <div className="flex flex-col h-screen justify-between">
+      <Header />
+      <div className="mb-auto">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
   )
 }
